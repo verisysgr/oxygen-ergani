67daaed67d0c54768ae5c379e5c201b8dda2c359
ogiritli <ogiritli@oxygen.gr>
2025-02-19 00:41:03 +0200

    Add tests for authentication failure scenarios
    Introduce test cases to validate exception handling during failed authentication and token refresh. This ensures proper handling of `AuthenticationException` and `RefreshTokenExpiredException` for invalid credentials or expired tokens.

--- tests/Auth/AuthenticationLoginTest.php
+++ tests/Auth/AuthenticationLoginTest.php
@@ -4,6 +4,7 @@
 
 namespace Tests\Auth;
 
+use OxygenSuite\OxygenErgani\Exceptions\AuthenticationException;
 use OxygenSuite\OxygenErgani\Http\Auth\AuthenticationLogin;
 use Tests\TestCase;
 
@@ -14,6 +15,20 @@ class AuthenticationLoginTest extends TestCase
         $auth = new AuthenticationLogin();
         $auth->getConfig()->setHandler($this->mockResponse(200, 'authentication.json'));
         $response = $auth->handle('username', 'password');
+
+        $this->assertNotNull($response);
+        $this->assertSame('test-access-token', $response->accessToken);
+        $this->assertSame('test-refresh-token', $response->refreshToken);
+        $this->assertDates('2025-02-21T14:44:28.2731304+02:00', $response->refreshTokenExpiresAt);
+    }
+
+    public function test_failed_authentication(): void
+    {
+        $this->expectException(AuthenticationException::class);
+
+        $auth = new AuthenticationLogin();
+        $auth->getConfig()->setHandler($this->mockResponse(401));
+        $response = $auth->handle('wrong-username', 'wrong-password');
 
         $this->assertNotNull($response);
         $this->assertSame('test-access-token', $response->accessToken);
--- tests/Auth/AuthenticationRefreshTest.php
+++ tests/Auth/AuthenticationRefreshTest.php
@@ -4,6 +4,7 @@
 
 namespace Tests\Auth;
 
+use OxygenSuite\OxygenErgani\Exceptions\RefreshTokenExpiredException;
 use OxygenSuite\OxygenErgani\Http\Auth\AuthenticationRefresh;
 use Tests\TestCase;
 
@@ -13,6 +14,20 @@ class AuthenticationRefreshTest extends TestCase
     {
         $auth = new AuthenticationRefresh();
         $auth->getConfig()->setHandler($this->mockResponse(200, 'authentication.json'));
+        $response = $auth->handle('old-test-access-token', 'old-test-refresh-token');
+
+        $this->assertNotNull($response);
+        $this->assertSame('test-access-token', $response->accessToken);
+        $this->assertSame('test-refresh-token', $response->refreshToken);
+        $this->assertDates('2025-02-21T14:44:28.2731304+02:00', $response->refreshTokenExpiresAt);
+    }
+
+    public function test_authentication_failed_refresh(): void
+    {
+        $this->expectException(RefreshTokenExpiredException::class);
+
+        $auth = new AuthenticationRefresh();
+        $auth->getConfig()->setHandler($this->mockResponse(401));
         $response = $auth->handle('old-test-access-token', 'old-test-refresh-token');
 
         $this->assertNotNull($response);
